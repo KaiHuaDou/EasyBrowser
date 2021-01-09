4b11c75b5ebf937792b22a3a25190cb8fe55cd57
开花豆 <74781268+KaiHuaDou@users.noreply.github.com>
2021-01-09 17:38:39 +0800

    增加了实例获取函数

--- 极简浏览器/Api/BrowserCore.cs
+++ 极简浏览器/Api/BrowserCore.cs
@@ -5,15 +5,19 @@ namespace 极简浏览器.Api
 {
     public static class BrowserCore
     {
-        public static void Navigate(string url)
+        public static MainWindow GetInstance( )
         {
             foreach (Window window in Application.Current.Windows)
             {
                 if(window is MainWindow)
                 {
-                    ((MainWindow) window).wb.Navigate(url);
+                    return ((MainWindow) window);
                 }
             }
+        }
+        public static void Navigate(string url)
+        {
+            GetInstance( ).wb.Navigate(url);
         }
     }
 }
